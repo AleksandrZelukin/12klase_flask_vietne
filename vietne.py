2728b5c4a3c09c9acb424f59dee57d05a41b0039
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-02-19 13:12:11 +0200

    Update apps

--- vietne.py
+++ vietne.py
@@ -4,7 +4,15 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/blog")
+def blog():
+    return render_template("blog.html") 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5050)
