95ed51c3bd260a23fe5c1d925d6900dd14fe7b87
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-02-24 10:55:52 +0200

    Update apps

--- vietne.py
+++ vietne.py
@@ -3,16 +3,16 @@ from flask import *
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",title="Sveiki!")
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title="PAR")
 @app.route("/blog")
 def blog():
-    return render_template("blog.html") 
+    return render_template("blog.html",title="BLOGS") 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html",title="KONTAKTI")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5050)
